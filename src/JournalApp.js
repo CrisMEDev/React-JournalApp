5d08426448893d4caea53b8ea53d9ccdb14b30b6
Cristian Morales <cr15moresp@gmail.com>
2021-10-01 16:08:58 -0500

    CCreación de la estructura inicial del proyecto JournalApp e implementación de rutas

---
+++ src/JournalApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { AppRouter } from './routers/AppRouter';
+
+import './styles/styles.scss';
+
+export const JournalApp = () => {
+    return (
+        <>
+            <AppRouter />
+        </>
+    )
+}
