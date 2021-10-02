869ab24ec92a6afc58ea7f353bec41b61edafcbd
Cristian Morales <cr15moresp@gmail.com>
2021-10-01 23:22:08 -0500

    Estilos en authRouter implementados

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,32 @@ import React from 'react';
 export const LoginScreen = () => {
     return (
         <>
-            <h1>Login screen</h1>
+            <h3>Login</h3>
+
+            <form>
+
+                <input
+                    type="text"
+                    placeholder="email"
+                    name="email"
+                />
+
+                <input
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                />
+
+                <button
+                    type="submit"
+                >
+                    Login
+                </button>
+
+                <hr />
+                Google
+
+            </form>
         </>
     )
 }
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -11,14 +11,16 @@ import { RegisterScreen } from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
     return (
-        <div>
-            <Switch>
-                
-                <Route exact path="/auth/login" component={ LoginScreen } />
-                <Route exact path="/auth/register" component={ RegisterScreen } />
-                <Redirect to="/auth/login" />
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    
+                    <Route exact path="/auth/login" component={ LoginScreen } />
+                    <Route exact path="/auth/register" component={ RegisterScreen } />
+                    <Redirect to="/auth/login" />
 
-            </Switch>
+                </Switch>
+            </div>
         </div>
     )
 }
