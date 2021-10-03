a5023b735c7b498aa553451b15c811b9ca6f700b
Cristian Morales <cr15moresp@gmail.com>
2021-10-02 21:12:53 -0500

    Diseño del home screen realizado

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -8,6 +8,29 @@ export const NoteScreen = () => {
             
                 <NotesAppbar />
 
+                <div className="notes__content">
+
+                    <input
+                        type="text"
+                        placeholder="Some good title"
+                        className="notes__title_input"
+                        autoComplete="off"
+                    />
+
+                    <textarea
+                        placeholder="What do you need to write today?"
+                        className="notes__textarea"
+                    ></textarea>
+
+                    <div className="notes__image">
+                        <img
+                            src="https://i.pinimg.com/474x/cf/8c/9d/cf8c9d075a37c13302f0cb5f6379a508.jpg"
+                            alt="Cannot show"
+                        />
+                    </div>
+
+                </div>
+
         </div>
     )
 }
