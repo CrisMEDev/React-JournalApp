acbcf1544956e5fbc6cafc1a5e78a8856bac2d8a
Cristian Morales <cr15moresp@gmail.com>
2021-10-03 00:08:11 -0500

    Instalaciones y configuraciones de react reduc en la aplicación realizadas

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Para trabajar con SASS usar: `npm install --save-dev sass`
 
 [SASS](https://www.npmjs.com/package/sass)
 [npm install react-router-dom](https://reactrouter.com/web/guides/quick-start)
+[redux](https://es.redux.js.org/)
+[react-redux](https://react-redux.js.org/introduction/getting-started)
--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import { AppRouter } from './routers/AppRouter';
+import { store } from './store/store';
 
 import './styles/styles.scss';
 
 export const JournalApp = () => {
     return (
-        <>
+        // Se carga el store redux a lo largo de toda la aplicación
+        <Provider store={ store }>
             <AppRouter />
-        </>
+        </Provider>
     )
 }
