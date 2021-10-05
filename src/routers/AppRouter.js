b24b0d153c507dc58c00c8aadc6aa8198cc2870d
Cristian Morales <cr15moresp@gmail.com>
2021-10-05 17:19:01 -0500

    Usado un useEffect y dispatch en el router para mantener el estado del logeo en la aplicacion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Route,
     Switch,
     Redirect
 } from 'react-router-dom';
+import { getAuth } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
 
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        
+        const auth = getAuth();
+
+        auth.onAuthStateChanged( (user) => {    // Si no se está autenticado, user es null
+
+            if ( user?.uid ){
+                dispatch( login( user.uid, user.displayName ) );
+            }
+
+        });  // El método crea un observable; se dispra cuando el usuario
+             // cada que el usuario haga una accion con login
+
+    }, [dispatch]);
+
     return (
         <Router>
             <div>
